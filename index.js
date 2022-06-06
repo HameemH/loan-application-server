c334a0243d0961e65d7d7cafe23efdb9982c9ab2
HameemH <hameemhossain2@gmail.com>
2022-06-07 01:17:50 +0600

    commiting for deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -20,7 +20,37 @@ async function run (){
   
             const addusers = await userCollection.insertOne(usersInfo);
             res.send(addusers)
-          })
+          });
+          app.get('/users', async (req,res)=>{
+            
+            const query ={};
+            const cursor = userCollection.find(query)
+            const users = await cursor.toArray();
+            
+            res.send(users)
+        })
+        app.get('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        })
+        app.put('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const usersInfo = req.body;
+            console.log(usersInfo);
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: 
+                   usersInfo
+                
+            };
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
+        })
+
     }
     finally{
 
