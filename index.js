a3dfc6679b1d9a9a6818a925d8a3c4c62fc449ee
HameemH <hameemhossain2@gmail.com>
2022-06-07 01:43:53 +0600

    solving

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const corsConfig = {
-    origin: 'http://localhost:3001/',
+    origin: 'http://localhost:3001',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
